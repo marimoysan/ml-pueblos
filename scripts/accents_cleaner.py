7886e20b2328964b3d163e3a708a0af89099e499
kayoken <kayoken@users.noreply.github.com>
2025-02-11 12:33:06 +0100

    eda hospitals (#6)

---
+++ scripts/accents_cleaner.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+class AccentsCleaner:
+    """A class to clean dataframes from any accents"""
+
+    def __init__(self, dataframes: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    # go over each df -> look a accents -> replace the accents with english equivalent
