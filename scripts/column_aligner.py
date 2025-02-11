2acfd3bb2317c0cb1f82b65515a48e1aded2df18
kayoken <kayoken@users.noreply.github.com>
2025-02-11 19:14:57 +0100

    fixing different typings in columns (#6)

--- scripts/column_aligner.py
+++ scripts/column_aligner.py
@@ -16,4 +16,18 @@ class ColumnAligner:
         pass
 
     def alignColumns(self):
-        pass
+        # TODO make this more flexible by using the pattern, no hardcode
+        commas = self.boss.municipality_name_clean.str.contains(r".*, ")
+        extract_list = self.boss[commas][self.column].str.split(",").str[0].to_list()
+
+        for elem in extract_list:
+            # Create a mask for rows in df_coordinates that contain the element
+            mask = self.minion[self.column].str.contains(
+                re.escape(elem) + r" \(", case=False, na=False
+            )
+            # # Get the corresponding full name from df_communities
+            full_name = self.boss[
+                self.boss[self.column].str.contains(f"{elem},", case=False, na=False)
+            ][self.column].iloc[0]
+            # Update df_coordinates with the full name where mask is True
+            self.minion.loc[mask, self.column] = full_name
