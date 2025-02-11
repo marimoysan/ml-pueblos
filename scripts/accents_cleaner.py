4c0f28edf454d56a9676e162eb5db5db4296e706
kayoken <kayoken@users.noreply.github.com>
2025-02-11 15:21:21 +0100

    removed accents and merged dataframes (#15)

--- scripts/accents_cleaner.py
+++ scripts/accents_cleaner.py
@@ -1,14 +1,50 @@
 import pandas as pd
 import numpy as np
 
+accents = {
+    "á": "a",
+    "é": "e",
+    "è": "e",
+    "í": "i",
+    "ó": "o",
+    "ú": "u",
+    "ñ": "n",
+    "ü": "u",
+    "¡": "i",
+    "Á": "A",
+    "É": "E",
+    "È": "E",
+    "Í": "I",
+    "Ó": "O",
+    "Ú": "U",
+    "Ñ": "N",
+    "Ü": "U",
+    "¿": "?",
+}
+
 
 class AccentsCleaner:
     """A class to clean dataframes from any accents"""
 
-    def __init__(self, dataframes: pd.DataFrame) -> pd.DataFrame:
-        pass
+    def __init__(
+        self,
+        dataframes: pd.DataFrame,
+        columns: list[str],
+        replacement_dict: dict = accents,
+    ):
+        self.dataframes = dataframes
+        self.columns = columns
+        self.replacement_dict = replacement_dict
 
     def __repr__(self) -> str:
         pass
 
-    # go over each df -> look a accents -> replace the accents with english equivalent
+    def clean(self):
+        for df in self.dataframes:
+            for column in self.columns:
+                if column in df.columns:
+                    df[column + "_clean"] = df[column].apply(
+                        lambda x: "".join(
+                            self.replacement_dict.get(c, c) for c in str(x)
+                        ).lower()
+                    )
