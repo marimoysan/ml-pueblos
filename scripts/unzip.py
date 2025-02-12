86dd7f646164d76d9c7443908be09addfca1d1e3
kayoken <kayoken@users.noreply.github.com>
2025-02-12 17:59:38 +0100

    (#12) built script for extracting all files in resources folder

--- scripts/unzip.py
+++ scripts/unzip.py
@@ -1,6 +1,9 @@
 import zipfile
+import glob
 
-path_to_zip_file = "../data.zip"
+path_to_zip_file = "../resources/*"
 
-with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
-    zip_ref.extractall("../data/raw")
+zip_files = glob.glob(path_to_zip_file)
+for path_to_zip_file in zip_files:
+    with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
+        zip_ref.extractall("../data/raw")
