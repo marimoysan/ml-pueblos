e44cc5e7cc5087e3f8da143013d01c541ea7be3b
kayoken <kayoken@users.noreply.github.com>
2025-02-14 17:18:15 +0100

    (#11) multicore locator

--- scripts/locator.py
+++ scripts/locator.py
@@ -1,34 +1,56 @@
 import pandas as pd
 from geopy.distance import geodesic
+from pandarallel import pandarallel
+pandarallel.initialize()
 
+import multiprocessing.pool
+
+class NonDaemonProcess(multiprocessing.Process):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.daemon = False
+
+    @property
+    def daemon(self):
+        return False
+
+    @daemon.setter
+    def daemon(self, value):
+        pass
+
+class MyPool(multiprocessing.pool.Pool):
+    Process = NonDaemonProcess
+
+# (Optional) Remove this patch if pandarallel already handles process creation.
+# Locator.pool = MyPool  
+
+def find_nearest_location(row, df_destination, destination_name):
+    """Module-level function to compute the nearest destination."""
+    origin = (row['latitude'], row['longitude'])
+    distances = df_destination.apply(
+        lambda x: geodesic(origin, (x['latitude'], x['longitude'])).kilometers, axis=1
+    )
+    nearest_index = distances.idxmin()
+    nearest_distance = distances[nearest_index]
+    nearest_location = df_destination.loc[nearest_index, destination_name]
+    return pd.Series([nearest_distance, nearest_location])
 
 class Locator:
     """
-        Locatorclass
+    Locator class that calculates the closest distance and location.
     """
     def __init__(self, origin_location, df_destination, destination_name):
-        """_summary_
-
-        Args:
-            origin_location (_type_): DataFrame from where to calculate
-            df_destination (_type_): DataFrame with destinatin
-            destination_name (_type_): location name for closest point
-
-        Returns:
-            _type_: returns nearest distance and nearest location in series
         """
+        Args:
+            origin_location (DataFrame): DataFrame with origin points.
+            df_destination (DataFrame): DataFrame with destination points.
+            destination_name (str): Column name in df_destination holding the location's name.
+        """
+        self.origin_location = origin_location
         self.df_destination = df_destination
         self.destination_name = destination_name
-        self.origin_location = origin_location
-        self.result = self.origin_location.apply(lambda row: self._find_nearest_location(row), axis=1)
 
-    def _find_nearest_location(self, row):
-        self.origin_location = (row['latitude'], row['longitude'])
-        # Calculate distance from the town to every airport
-        distances = self.df_destination.apply(lambda x: geodesic(self.origin_location, (x['latitude'], x['longitude'])).kilometers, axis=1)
-        # Find the index of the airport with the smallest distance
-        nearest_index = distances.idxmin()
-        # Retrieve the minimum distance and corresponding airport ID
-        nearest_distance = distances[nearest_index]
-        nearest_location = self.df_destination.loc[nearest_index, self.destination_name]
-        return pd.Series([nearest_distance, nearest_location])
+        # Use parallel_apply with a module-level function.
+        self.result = self.origin_location.parallel_apply(
+            find_nearest_location, axis=1, args=(self.df_destination, self.destination_name)
+        )
