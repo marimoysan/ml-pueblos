101330627fcfd3ea7ef382062ff1d55354cfe57e
kayoken <kayoken@users.noreply.github.com>
2025-02-25 19:49:25 +0100

    (#19) - ohe

--- frontend/pages/2_Machine_Learning.py
+++ frontend/pages/2_Machine_Learning.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.compose import ColumnTransformer
-
 import streamlit as st
 
 pd.set_option("display.max_columns", None)
@@ -15,48 +14,42 @@ pd.set_option("display.max_columns", None)
 plt.style.use(
     "https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-light.mplstyle"
 )
+# Define default session state values
+default_session_state = {
+    "df_select": pd.DataFrame(),
+    "columns": pd.DataFrame(),
+    "another_key": "default_value",
+}
+# Initialize session state
+for key, value in default_session_state.items():
+    if key not in st.session_state:
+        st.session_state[key] = value
+
 st.set_page_config(page_title="Machine Learning", page_icon="📈")
 st.sidebar.header("Machine Learning Demo")
 
 
-if "df_select" not in st.session_state:
-    st.session_state["df_select"] = pd.DataFrame()
-
-if "columns" not in st.session_state:
-    st.session_state["columns"] = pd.DataFrame()
-
-
-ohe = OneHotEncoder(drop="first", sparse_output=False)
-scaler = StandardScaler()
-
-pipeline = Pipeline(steps=[("ohe", OneHotEncoder(drop="first", sparse_output=False))])
-
-pipeline.fit(
-    st.session_state["df_select"][["koppen_climate", "town_size", "final_age_category"]]
+# Define categorical and numerical features based on the dataframe
+categorical_features = st.session_state["columns"].columns.to_list()
+numerical_features = (
+    st.session_state["df_select"].select_dtypes(include=[np.number]).columns.tolist()
 )
 
+# Build the pipeline for the categorical transformations
+pipeline = Pipeline(steps=[("ohe", OneHotEncoder(drop="first", sparse_output=False))])
+
+# Create the column transformer using the pipelines defined above.
 fe_transformer = ColumnTransformer(
     transformers=[
-        ("transf_cat", pipeline, ["koppen_climate"])(
-            "scaled",
-            scaler,
-            [
-                "connectivity_score",
-                "economy_score",
-                "economy_score_area",
-                "hospital_distance_score",
-                "school_distance_score",
-                "hospital_score_area",
-                "school_score_area",
-                "train_distance_score",
-                "airport_distance_score",
-                "transport_score",
-            ],
-        ),
+        ("transf_cat", pipeline, categorical_features),
+        ("scaled", StandardScaler(), numerical_features),
     ],
     remainder="drop",
 )
 
+# Fit the transformer to the dataframe
 fe_transformer.fit(st.session_state["df_select"])
 
 df_train = fe_transformer.transform(st.session_state["df_select"])
+
+st.dataframe(df_train)
